a6fbfc7c5fcc7a689b23a3d61300832b2883c744
inhalt <56723288+inhaltone@users.noreply.github.com>
2020-05-18 18:55:09 +0300

    spacecraft prototype

---
+++ js/space-craft.js
@@ -0,0 +1,25 @@
+
+/*
+$(document).ready(function(){
+var navBar = $('.navbar').height();
+
+$('#space-craft-nav').css('margin-top',navBar);
+
+});
+*/
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
